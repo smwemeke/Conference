5aabbf7ddbdb52ebcde96df71e285443fd763782
smwemeke <sue.mwemeke@gmail.com>
2024-05-23 21:17:03 -0500

    sql statements

--- sql queries.sql
+++ sql queries.sql
@@ -23,11 +23,24 @@ CREATE TABLE authorities(
    UPDATE USERS
    SET password = '$2a$10$GXPJ./tdqHsVpBwPDIOIeeI51rrwETO2naVBdp8iQNh78arVFO8nm'
    WHERE username = 'admin';
+   
+   CREATE TABLE persistent_logins(
+   username VARCHAR(50) NOT NULL,
+   series VARCHAR(64) PRIMARY KEY,
+   token VARCHAR (64) NOT NULL,
+   last_used TIMESTAMP NOT NULL,
+     FOREIGN KEY (username)  REFERENCES users(username)
+   );
     
    SELECT * FROM users; 
    
 ALTER TABLE USERS 
 ADD nickname VARCHAR(50);
+
+UPDATE USERS 
+SET nickname = 'matama' WHERE username = 'admin';
+
+
    
    commit
     
